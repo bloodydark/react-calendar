9deae40ba9c02b0956f82e0edc400545e1a77474
bloodydark <justiceremain48694802@gmail.com>
2020-12-15 19:31:39 +0900

    昨日を強調

--- src/SimpleDatePicker.jsx
+++ src/SimpleDatePicker.jsx
@@ -12,14 +12,20 @@ const SimpleDatePicker = () => {
     now.getMonth(),
     now.getDate() - 1
   );
-  console.log(yesterday);
+  const display = () => {
+    alert("Hey!");
+  };
+  console.log(startDate);
 
   return (
     <DatePicker
       selected={startDate}
+      //   onChange={(date) => setStartDate(date)}
+      //   onChange={display}
       onChange={(date) => setStartDate(date)}
       highlightDates={[yesterday]}
       placeholderText="This highlights a week ago and a week from today"
+      inline
     />
   );
 };
