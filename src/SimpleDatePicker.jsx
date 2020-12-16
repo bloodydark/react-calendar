b938030efb7c25a60d6560f21edc443eba943722
bloodydark <justiceremain48694802@gmail.com>
2020-12-16 12:52:18 +0900

    複数highlight成功

--- src/SimpleDatePicker.jsx
+++ src/SimpleDatePicker.jsx
@@ -7,15 +7,35 @@ import { subDays } from "date-fns";
 const SimpleDatePicker = () => {
   const [startDate, setStartDate] = useState(new Date());
   const now = new Date();
+  const array = [];
   const yesterday = new Date(
     now.getFullYear(),
     now.getMonth(),
     now.getDate() - 1
   );
+  array.push(yesterday);
+  // console.log(array);
+
+  const weekAgo = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() - 6
+  );
+  console.log(weekAgo);
+  array.push(weekAgo);
+  console.log(array);
+  // const test = [
+  //   { today: new Date() },
+  //   {
+  //     yesterday: new Date(now.getFullYear(), now.getMonth(), now.getDate() - 1),
+  //   },
+  // ];
+  // console.log(...test);
+
   const display = () => {
     alert("Hey!");
   };
-  console.log(startDate);
+  // console.log(startDate);
 
   return (
     <DatePicker
@@ -23,7 +43,7 @@ const SimpleDatePicker = () => {
       //   onChange={(date) => setStartDate(date)}
       //   onChange={display}
       onChange={(date) => setStartDate(date)}
-      highlightDates={[yesterday]}
+      highlightDates={array}
       placeholderText="This highlights a week ago and a week from today"
       inline
     />
